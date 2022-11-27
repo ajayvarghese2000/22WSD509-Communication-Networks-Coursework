0327cb454a0774fca516d1675cd5410820c4228e
ajayvarghese2000 <58085441+ajayvarghese2000@users.noreply.github.com>
2022-11-27 16:38:36 +0000

    Updated TCP to keep connection open until close

--- TCP/client.py
+++ TCP/client.py
@@ -5,7 +5,15 @@ PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+    
+    # Send Hello World 100 times to the server
+    for i in range(100):
 
-print(f"Received {data!r}")
+        s.sendall(b"Hello World")
+        
+    # Receive the data from the server
+    while True:
+        data = s.recv(1024)
+        if not data:
+            break
+        print(f"Received {data!r}")
--- TCP/server.py
+++ TCP/server.py
@@ -10,11 +10,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     delay_before_sending_upon_acceptance(s, 20, 1024)
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+    
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
