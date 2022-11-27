742a7463b81e52c8d45f4467e6b460d00bd1147e
ajayvarghese2000 <58085441+ajayvarghese2000@users.noreply.github.com>
2022-11-27 16:08:17 +0000

    Updated TCP Server to allow for connection delays

--- TCP/server.py
+++ TCP/server.py
@@ -1,10 +1,13 @@
 import socket
+from poorconn import make_socket_patchable, delay_before_sending_upon_acceptance
 
 HOST = "198.244.190.230" 
 PORT = 65432
 
 # TCP socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s = make_socket_patchable(s)
+    delay_before_sending_upon_acceptance(s, 20, 1024)
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
